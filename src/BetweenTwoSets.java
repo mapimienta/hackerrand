cf0179f106994e87bf9945a4330cafa041fb1197
Miguel Pimienta <mp@eljovitechnologies.net>
2016-11-08 09:08:13 -0500

    updates

--- src/BetweenTwoSets.java
+++ src/BetweenTwoSets.java
@@ -11,6 +11,7 @@ public class BetweenTwoSets {
         int n = in.nextInt();
         int m = in.nextInt();
         int[] a = new int[n];
+        int menor_b=0;
         int contador = 0;        
         for(int a_i=0; a_i < n; a_i++){
             a[a_i] = in.nextInt();
@@ -22,20 +23,26 @@ public class BetweenTwoSets {
         int[] b = new int[m];
         for(int b_i=0; b_i < m; b_i++){
             b[b_i] = in.nextInt();
-        }
+            if(b_i == 0){
+                menor_b = b[b_i];
+            }
+
+            if(b[b_i]<menor_b){
+                menor_b = b[b_i];
+            }                  
+        }                 
         
-        
-        int nb = getFactor2(b,0);
+        int nb = getFactor2(b,0,menor_b, na);
         //System.out.println(nb);
                 
-        int temp = na;
+        /*int temp = na;
         while (na <= nb) {            
             if(nb%na==0)
                 contador++;
             na = na + temp;            
-        }  
+        } */ 
         
-        System.out.println(contador);
+        System.out.println(nb);
     }
     
     static int getFactor(int[] a, int i) {
@@ -62,7 +69,9 @@ public class BetweenTwoSets {
         return retorno;
     }
     
-    static int getFactor2(int[] a, int i) {
+    static int getFactor2(int[] a, int i, int menor, int fact) {
+
+        
         if (i < a.length) {
             for (int j = 0; j < a.length; j++) {
                 //System.out.println(a[i]+":"+a[j]);
@@ -75,14 +84,31 @@ public class BetweenTwoSets {
                 }
             }
             i++;
-            getFactor2(a, i);
+            getFactor2(a, i, menor,fact);
         }
-        int retorno = 1;
-        for (int h = 0; h < a.length; h++) {
-            if (a[h] != 0) {
-                retorno = retorno * a[h];
-            }            
+    int retorno = 0;        
+                //retorno = retorno * a[h];
+    int temp = fact;
+    boolean factor_=true;
+        while(temp<=menor){
+            factor_ = true;
+            for (int h = 0; h < a.length; h++) {
+                if (a[h] != 0) {
+                    if(a[h]%temp==0){
+                        factor_ = true;
+                    }else{
+                        factor_=false;
+                        break;
+                    }
+                }                         
+            }
+            if(factor_==true){
+                
+                retorno++;
+            }
+            temp+=temp;
         }
+    
         return retorno;
     }    
 }
