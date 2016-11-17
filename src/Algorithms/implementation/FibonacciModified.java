443ee8a1cd0ea95e9de8b5c8f1385443c62f9f76
Miguel Pimienta <mp@eljovitechnologies.net>
2016-11-17 14:26:13 -0500

    fibonacci

---
+++ src/Algorithms/implementation/FibonacciModified.java
@@ -0,0 +1,29 @@
+package Algorithms.implementation;
+import java.util.Scanner;        
+
+public class FibonacciModified {
+    static int ini =3;
+    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int t1 = in.nextInt();
+        int t2 = in.nextInt();
+        int n = in.nextInt();
+        double[] fbarray=new double[n];        
+        fbarray[1]=0;        
+        fbarray[2]=1;
+        
+        fDP(n,fbarray);
+    }
+    
+
+    
+    static public void fDP(int n, double[] fb_array){
+        if(ini<=n){
+            fb_array[n]=fb_array[n-2] + Math.pow(fb_array[n-1],2);
+            ini++;
+            fDP(n,fb_array);
+        }
+    }
+}
+
